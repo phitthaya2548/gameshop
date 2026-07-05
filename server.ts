46b8056b5cd00c0aa6599a5d750283d86bb497a9
พิทยา แก้วไข่สอน <119053972+phitthaya2548@users.noreply.github.com>
2026-07-06 00:47:08 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import http from "http";
 import { app } from "./app";
 
-const port = process.env.PORT || 3000;  // ✅ แก้ตัวพิมพ์ใหญ่
+const port = process.env.PORT || 3000;  // 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log(`Server is started on port ${port}`); // ✅ print ค่าจริง จะได้ debug ง่ายขึ้นครั้งหน้า
+  console.log(`Server is started on port ${port}`); // 
 });
